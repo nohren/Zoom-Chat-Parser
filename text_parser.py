4b57951d9e9da75693744d3b9dfbe7d1bfebe097
Oren Nelson <orendnelson@gmail.com>
2021-01-24 02:27:01 -0800

    Update text_parser.py

--- text_parser.py
+++ text_parser.py
@@ -4,15 +4,14 @@ from pandasgui import show
 import re
 
 def topic_parser(file_path, topic_array):
-    #init topic array, the shorter the better, longer words are more specific
-    # topic_array = ['sleep', 'food', ' eat ', 'relax', 'workout', 'hack reactor', 'imposter', 'help desk', 'water', 'docs']
+    # example topics to search 'sleep', 'food', ' eat ', 'relax', 'workout', 'hack reactor', 'imposter', 'help desk', 'water', 'docs'
 
     topics = {}
     with open(file_path, 'r') as f:
         for line in f:
             line = line.strip()
             for i in topic_array:
-                if (re.search(i, line, re.IGNORECASE) != None):      #alternate is line.find(i) != -1 but it is case sensitive
+                if (re.search(i, line, re.IGNORECASE) != None):     
                     if (i in topics.keys()):
                         topics[i].append(line)
                     else:
@@ -21,8 +20,6 @@ def topic_parser(file_path, topic_array):
     return topics
 
 def arrayEqualizer(topics):
-    #need all arrays the same length so consider all dictionary property arrays, find one with longest length
-    #now consider them all again and append until length is == longest length with '--' chars
     longest_length = 0
     for key in topics:
        if (len(topics[key]) > longest_length):
@@ -43,10 +40,6 @@ def arrayEqualizer(topics):
 
 
 def printout(topics):
-    #intakes a topics dictionary, outputs a human readable printout
-    #convert dictionary to dict to pandas dataFrame
-    #print dataframe
-
     df = pd.DataFrame.from_dict(topics, orient='columns')
     show(df)
        
